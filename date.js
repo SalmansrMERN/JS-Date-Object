f4357a916627f1c890b919891346d44771bf7055
Salman S. Rahman <salmansr1995@gmail.com>
2021-12-11 14:18:07 +0600

    first commit

---
+++ date.js
@@ -0,0 +1,17 @@
+const date = new Date();
+console.log(date);
+
+const year = date.getFullYear();
+console.log("Year = " + year);
+
+const day = date.getDay();
+console.log("day = " + day);
+
+const hours = date.getHours();
+console.log("hours = " + hours);
+
+const minutes = date.getMinutes();
+console.log("minutes = " + minutes);
+
+const seconds = date.getSeconds();
+console.log("seconds = " + seconds);
